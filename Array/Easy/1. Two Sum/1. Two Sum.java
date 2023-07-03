2cf564dea487c2c5845748060b6cf17888a73a37
Tapish Rawat <iamtapishrawat@gmail.com>
2023-07-03 18:50:46 +0530

    Update 1. Two Sum.java

--- Array/Easy/1. Two Sum/1. Two Sum.java
+++ Array/Easy/1. Two Sum/1. Two Sum.java
@@ -1,1 +1,18 @@
-
+// Time complexity:O(n)
+// Space complexity:O(n)
+class Solution {
+    public int[] twoSum(int[] nums, int target) {                    
+         Map<Integer,Integer> map = new HashMap<>();
+        int []result = new int[2];
+        for(int i = 0;i<nums.length;i++){
+           if(map.containsKey(target - nums[i])){
+                result[1]=i;
+                result[0]=map.get(target-nums[i]);
+                break;
+            }
+            map.put(nums[i],i);
+        }
+        return result;
+        
+    }
+}
