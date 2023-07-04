5ef0d96cb93f3b78f3278bf8a74846d1789bebf7
Tapish Rawat <iamtapishrawat@gmail.com>
2023-07-04 18:32:14 +0530

    Create 3Sum.java

---
+++ Array/Medium/974. Subarray Sums Divisible by K/3Sum/3Sum.java
@@ -0,0 +1,28 @@
+// Time Complexity : O(N^2)
+// Space Complexity : O(3*k)  // k is the no.of triplets
+class Solution {
+        public List<List<Integer>> threeSum(int[] num) {
+        Arrays.sort(num);
+        List < List < Integer >> res = new ArrayList < > ();
+        for (int i = 0; i < num.length - 2; i++) {
+            if (i == 0 || (i > 0 && num[i] != num[i - 1])) {
+                int lo = i + 1, hi = num.length - 1, sum = 0 - num[i];
+                while (lo < hi) {
+                    if (num[lo] + num[hi] == sum) {
+                        List < Integer > temp = new ArrayList < > ();
+                        temp.add(num[i]);
+                        temp.add(num[lo]);
+                        temp.add(num[hi]);
+                        res.add(temp);
+                        while (lo < hi && num[lo] == num[lo + 1]) lo++;
+                        while (lo < hi && num[hi] == num[hi - 1]) hi--;
+                        lo++;
+                        hi--;
+                    } else if (num[lo] + num[hi] < sum) lo++;
+                    else hi--;
+                }
+            }
+        }
+        return res;
+        }
+    }
