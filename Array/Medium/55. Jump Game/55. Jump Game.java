0ac3fb38648d8d28c90e400a27f2e18e9485d113
Tapish Rawat <iamtapishrawat@gmail.com>
2023-07-05 18:58:49 +0530

    Update 55. Jump Game.java

--- Array/Medium/55. Jump Game/55. Jump Game.java
+++ Array/Medium/55. Jump Game/55. Jump Game.java
@@ -1,1 +1,27 @@
+// Greedy Approach
+//   Time complexity: O(n)
+//   Space complexity: O(1)
+class Solution {
+    public boolean canJump(int[] nums) {
+        int goal = nums.length-1;
+       for(int i = goal-1; i >= 0; i--){
+           if(nums[i]+i >= goal){
+               goal = i;
+           }
+       }
+       return goal == 0;
+    }
+}
 
+
+class Solution {
+    public boolean canJump(int[] nums) {
+        int maxDist = 0;
+
+        for(int i = 0; i < nums.length; i++) {
+            if(i > maxDist) return false;
+            maxDist = Math.max(i + nums[i], maxDist);
+        }
+        return true;
+    }
+}
