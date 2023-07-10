bcbaac881fe496b3f32a02bcb124c2cb4e7ab6b6
Tapish Rawat <iamtapishrawat@gmail.com>
2023-07-10 23:09:54 +0530

    Create 680. Valid Palindrome II.java

---
+++ String/Easy/680. Valid Palindrome II/680. Valid Palindrome II.java
@@ -0,0 +1,28 @@
+class Solution{
+    public boolean validPalindrome(String s) {
+    
+    int i=0,j=s.length()-1;
+    
+    while(i<j)
+    {
+       if(s.charAt(i)!=s.charAt(j))         //Check if characters in the middle between (i+1 & j) OR (i & j-1) are palindromes
+           return isPalindrome(s,i+1,j)||isPalindrome(s,i,j-1);
+        i++;
+        j--;
+    }
+    return true;
+}
+
+public boolean isPalindrome(String s, int i, int j)
+{
+    while(i<j)
+    {
+        if(s.charAt(i)!=s.charAt(j))
+            return false;
+        i++;
+        j--;
+    }
+    return true;
+        
+}
+}
